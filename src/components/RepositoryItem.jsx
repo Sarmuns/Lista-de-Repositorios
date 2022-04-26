b19d355a608cff8a302d4d1fe019399b22dda4c5
Sarmuns <samuel.afrocha@gmail.com>
2022-04-26 18:41:50 -0300

    Usando operador '??' caso não haja descrição

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -2,7 +2,7 @@ export function RepositoryItem(props){
     return(
         <li>
         <strong>{props.repo.name}</strong>
-        <p>{props.repo.description}</p>
+        <p>{props.repo.description ?? 'Repositório sem descrição'}</p>
 
         <a type="button" target= "_blank" href={props.repo.html_url}>
             Acessar Repo
