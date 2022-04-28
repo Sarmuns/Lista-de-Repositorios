eae88061e4b80714970b7f40e6eb381d3900e25d
Sarmuns <samuel.afrocha@gmail.com>
2022-04-27 21:35:10 -0300

    diamond emoji :) + header

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react"
+
 export function RepositoryItem(props){
+
+
+
+    //props.repo.name=='Pokedex'?'Pokedex :)':props.repo.name
+
     return(
         <li>
-        <strong>{props.repo.name}</strong>
+        <strong>{props.repo.name} &#128142;</strong>
         <p>{props.repo.description ?? 'Repositório sem descrição'}</p>
 
         <a type="button" target= "_blank" href={props.repo.html_url}>
